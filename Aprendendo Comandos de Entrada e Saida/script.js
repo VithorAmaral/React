c8dd6ef8336cd501a7115510fa4d679abd535497
Vithor Amaral <151647805+VithorAmaral@users.noreply.github.com>
2024-03-12 11:16:37 -0300

    Atualização

---
+++ Aprendendo Comandos de Entrada e Saida/script.js
@@ -0,0 +1,5 @@
+var idade = prompt("Digite sua idade:");
+document.write("<h1>Aprendendo JavaScript</h1>");
+document.write("</br>");
+document.write("<img src='https://upload.wikimedia.org/wikipedia/pt/e/ed/Shrek%28personagem%29.jpg'/>")
+document.write("<strong>Aprendendo JavaScript </strong>")
