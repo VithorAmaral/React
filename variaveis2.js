3cfb8d8d45b4c2204470c122b0cc0905e9ec1031
Vithor Amaral <151647805+VithorAmaral@users.noreply.github.com>
2024-03-12 10:49:23 -0300

    Atualização

---
+++ variaveis2.js
@@ -0,0 +1,24 @@
+var a;
+a = 10;
+var b = 5;
+console.log(a);
+console.log(b);
+
+var nome = "Vithor";
+console.log(nome);
+//console.log(Nome);//
+
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+
+nome = "Lucas";
+console.log(nome);
+nome = "Matheus";
+console.log(nome);
+
+var temporaria;
+var sobrenome;
+sobrenome = "Silva";
+temporaria = nome + " " + sobrenome;
+console.log(temporaria);
