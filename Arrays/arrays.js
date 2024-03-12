0a3bf07d535764829d904dfb0f91b7c45d56e12e
Vithor Amaral <151647805+VithorAmaral@users.noreply.github.com>
2024-03-12 15:58:10 -0300

    Atualização

---
+++ Arrays/arrays.js
@@ -0,0 +1,11 @@
+var nome = "Vithor";
+console.log(nome);
+
+var lista = ["Matheus", "Lucas", "Henrique", "João", 15];
+console.log(lista);
+
+console.log(lista.length);
+
+console.log(lista[1]);
+console.log(lista[4]);
+console.log(lista[2]);
