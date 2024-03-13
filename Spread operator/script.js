563baf33dcec22d840b9cc2e7b94035cdc2ecf66
Vithor Amaral <151647805+VithorAmaral@users.noreply.github.com>
2024-03-13 10:40:25 -0300

    Atualização

--- Spread operator/script.js
+++ Spread operator/script.js
@@ -1,2 +1,12 @@
+function cadastroPessoa(info){
+    let novosDados = {
+        ...info,
+        cargo: "programador",
+        status: 1,
+        codigo: "6544645213"
+    };
+    return novosDados;
+}
 
+console.log(cadastroPessoa({nome: "Matheus", sobrenome: "Fraga", anoInicio: 2045 }));
 
