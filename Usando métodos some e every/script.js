2b52dd19338e4c18f86f79b381392f027730f378
Vithor Amaral <151647805+VithorAmaral@users.noreply.github.com>
2024-03-13 12:06:27 -0300

    Atualização

---
+++ Usando métodos some e every/script.js
@@ -0,0 +1,18 @@
+// SOME EVERY
+
+let nomes = [
+    { nome: "Matheus", idade: 18 },
+    { nome: "Maria", idade: 25 },
+    { nome: "Henrique", idade: 20 }
+];
+
+// console.log(nomes.some(nomes => nomes === "Maria"));
+
+console.log(nomes.every(nome => nome.idade >= 18));
+
+if(nomes.every(nome => nome.idade >= 28)) {
+    console.log("TODOS SÃO MAIORES DE 18")
+}else{
+    console.log("OPS ALGUEM É DE MENOR");
+}
+
