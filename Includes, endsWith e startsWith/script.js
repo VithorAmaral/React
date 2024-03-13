bebaa54a45886b6966ce4870b20c8f048d946a1b
Vithor Amaral <151647805+VithorAmaral@users.noreply.github.com>
2024-03-13 11:56:45 -0300

    Atualização

---
+++ Includes, endsWith e startsWith/script.js
@@ -0,0 +1,16 @@
+// includes
+// endsWith
+// startsWith
+
+// let nomes = ["Matheus", "Lucas", "João"];
+
+// console.log(nomes.includes("Luca"));
+
+
+let nome = "Matheus"
+
+// console.log(nome.endsWith("eus"));
+
+console.log(nome.startsWith("Mat"));
+
+
