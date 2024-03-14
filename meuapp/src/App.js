b60bc994eabe816b0f8f7e8828c719df32c9a021
Vithor Amaral <151647805+VithorAmaral@users.noreply.github.com>
2024-03-14 10:34:15 -0300

    Atualização

--- meuapp/src/App.js
+++ meuapp/src/App.js
@@ -1,8 +1,24 @@
-export default function App(){
+import React from 'react';
+
+const Bemvindo = (props) => {
   return(
     <div>
-      <h1>Bem vindo ao sistema</h1>
-      <h2>@SujeitoProgramador :)</h2>
+      <h2>Bem-vindo(a) {props.nome}</h2>
+      <h3>Tenho {props.anos} Anos</h3>
+
     </div>
-  )
+  );
 }
+
+
+function App(){
+  return(
+    <div>
+      Olá Mundo!
+      <Bemvindo nome="Vithor" anos="20"/>
+      <Bemvindo nome="Amanda" anos="21"/>
+    </div>
+  );
+}
+
+export default App;
