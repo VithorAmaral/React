6db6aec880d1b9bedd75197718bbc187ffce16fa
Vithor Amaral <151647805+VithorAmaral@users.noreply.github.com>
2024-03-19 11:50:19 -0300

    Atualização

--- formulario/src/App.js
+++ formulario/src/App.js
@@ -7,18 +7,47 @@ class App extends Component {
     this.state = {
       nome: "",
       email: "",
-      senha: ""
+      senha: "",
+      error: ""
     };
+
+    this.cadastrar = this.cadastrar.bind(this);
+  }
+
+  cadastrar(e) {
+    const { nome, email, senha } = this.state;
+
+    if (nome !== '' && email !== '' && senha !== '') {
+      alert(`Nome: ${nome} \nEmail: ${email} \nSenha: ${senha}`);
+    }else{
+      this.setState({error: "Ops! Parece que está faltando algo!"});
+    }
+
+
+
+    e.preventDefault();
   }
 
   render() {
     return (
       <div>
         <h1>Novo usuario</h1>
-
-        <form>
+        
+        <form onSubmit={this.cadastrar}>
+          <label>Nome: </label>
           <input type="text" value={this.state.nome}
-            onChange={ (e) => this.setState({nome: e.target.value})} />
+            onChange={(e) => this.setState({ nome: e.target.value })} />
+          <br />
+          <label>Email: </label>
+          <input type="email" value={this.state.email}
+            onChange={(e) => this.setState({ email: e.target.value })} />
+          <br />
+          <label>Senha: </label>
+          <input type="password" value={this.state.senha}
+            onChange={(e) => this.setState({ senha: e.target.value })} />
+          <br />
+          <button type="submit">Cadastrar</button>
+          {this.state.error && <p>{this.state.error}</p>}
         </form>
       </div>
     );
