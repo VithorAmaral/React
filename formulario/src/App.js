cccf910142af6a1b44306e04bb98836e81fc9f6c
Vithor Amaral <151647805+VithorAmaral@users.noreply.github.com>
2024-03-20 11:02:47 -0300

    Atualização

--- formulario/src/App.js
+++ formulario/src/App.js
@@ -2,58 +2,58 @@ import React, { Component } from 'react';
 
 class App extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      nome: "",
-      email: "",
-      senha: "",
-      error: ""
-    };
+    constructor(props) {
+        super(props);
+        this.state = {
+            form: {
+                nome: "",
+                email: "",
+                senha: "",
+                sexo: ""
+            }
+        }
+        this.dadosForm = this.dadosForm.bind(this);
 
-    this.cadastrar = this.cadastrar.bind(this);
-  }
+    }
 
-  cadastrar(e) {
-    const { nome, email, senha } = this.state;
-
-    if (nome !== '' && email !== '' && senha !== '') {
-      alert(`Nome: ${nome} \nEmail: ${email} \nSenha: ${senha}`);
-    }else{
-      this.setState({error: "Ops! Parece que está faltando algo!"});
+    dadosForm(e){
+        let form = this.state.form;
+        form[e.target.name] = e.target.value;
+        this.setState({form: form});
     }
 
 
 
-    e.preventDefault();
-  }
 
-  render() {
-    return (
-      <div>
-        <h1>Novo usuario</h1>
-        
-        <form onSubmit={this.cadastrar}>
-          <label>Nome: </label>
-          <input type="text" value={this.state.nome}
-            onChange={(e) => this.setState({ nome: e.target.value })} />
-          <br />
-          <label>Email: </label>
-          <input type="email" value={this.state.email}
-            onChange={(e) => this.setState({ email: e.target.value })} />
-          <br />
-          <label>Senha: </label>
-          <input type="password" value={this.state.senha}
-            onChange={(e) => this.setState({ senha: e.target.value })} />
-          <br />
-          <button type="submit">Cadastrar</button>
-          {this.state.error && <p>{this.state.error}</p>}
-        </form>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <h2>Login</h2>
+                Nome:
+                <input type="text" name="nome" value={this.state.form.nome}
+                    onChange={this.dadosForm} /> <br />
+                Email:
+                <input type="email" name="email" value={this.state.form.email}
+                    onChange={this.dadosForm} /> <br />
+                Senha:
+                <input type="text" name="senha" value={this.state.form.senha}
+                    onChange={this.dadosForm} /><br />
+
+                Sexo:
+                <select name="sexo" value={this.state.form.sexo} onChange={this.dadosForm}>
+                    <option value="masculino">Masculino</option>
+                    <option value="feminino">Feminino</option>
+                </select>
+
+                <div>
+                    <h3>{this.state.form.nome}</h3>
+                    <h3>{this.state.form.email}</h3>
+                    <h3>{this.state.form.senha}</h3>
+                    <h3>{this.state.form.sexo}</h3>
+                </div>
+            </div>
+        );
+    }
 }
-
-
 
 export default App;
