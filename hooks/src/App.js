74a40f090fd914bb1f4e89ab1b29a1427dc12dec
Vithor Amaral <151647805+VithorAmaral@users.noreply.github.com>
2024-03-22 11:41:08 -0300

    Atualização

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useMemo } from 'react';
+import React, { useState, useEffect, useMemo, useCallback } from 'react';
 
 
 function App() {
@@ -7,25 +7,25 @@ function App() {
   const [input, setInput] = useState('');
 
 
-  useEffect(()=>{
+  useEffect(() => {
     const tarefasStorage = localStorage.getItem('tarefas');
 
-    if(tarefasStorage){
+    if (tarefasStorage) {
       setTarefas(JSON.parse(tarefasStorage));
     }
 
   }, []);
 
 
-  useEffect(()=> {
+  useEffect(() => {
     localStorage.setItem('tarefas', JSON.stringify(tarefas));
   }, [tarefas]);
 
 
-  function handleAdd(){
+  const handleAdd = useCallback(() => {
     setTarefas([...tarefas, input])
     setInput('');
-  }
+  }, [tarefas, input]);
 
   const totalTarefas = useMemo(() => tarefas.length, [tarefas]);
 
@@ -37,9 +37,9 @@ function App() {
           <li key={tarefa}>{tarefa}</li>
         ))}
       </ul>
-      <br/>
-      <strong>Você tem {totalTarefas} tarefas!</strong><br/>
-      <input type="text" value={input} onChange={e => setInput(e.target.value)}/>    
+      <br />
+      <strong>Você tem {totalTarefas} tarefas!</strong><br />
+      <input type="text" value={input} onChange={e => setInput(e.target.value)} />
       <button type="button" onClick={handleAdd}>Adicionar</button>
 
     </div>
