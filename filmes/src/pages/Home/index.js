9762261a7c28e69fdc7e11ee89bbd1bba8ea8555
Vithor Amaral <151647805+VithorAmaral@users.noreply.github.com>
2024-03-28 11:55:38 -0300

    Atualização

--- filmes/src/pages/Home/index.js
+++ filmes/src/pages/Home/index.js
@@ -1,5 +1,7 @@
 import { useEffect, useState } from 'react';
 import api from '../../services/api';
+import { Link } from 'react-router-dom';
+import './home.css'
 
 // URL DA API: /movie/now_playing?api_key=e3c7ccb5f2c94e9b77e93e6ba3c1f318&language=pt-BR
 
@@ -9,14 +11,15 @@ function Home() {
     useEffect(() => {
         async function loadFilmes() {
             const response = await api.get("movie/now_playing", {
-                params:{
+                params: {
                     api_key: "e3c7ccb5f2c94e9b77e93e6ba3c1f318",
                     language: "pt-BR",
                     page: 1,
                 }
             })
 
-            console.log(response)
+            //console.log(response.data.results.slice(0,10));
+            setFilmes(response.data.results.slice(0, 10));
         }
 
         loadFilmes();
@@ -24,8 +27,18 @@ function Home() {
     }, []);
 
     return (
-        <div>
-            <h1>BEM VINDO A HOME</h1>
+        <div className='container'>
+            <div className='lista-filmes'>
+                {filmes.map((filme) => {
+                    return (
+                        <article key={filme.id}>
+                            <strong>{filme.title}</strong>
+                            <img src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`} alt={filme.title} />
+                            <Link to={`/filme/${filme.id}`}>Acessar</Link>
+                        </article>
+                    )
+                })}
+            </div>
         </div>
     )
 }
