0e61624edfd5dc41bd94a572772a977048ef956e
Vithor Amaral <151647805+VithorAmaral@users.noreply.github.com>
2024-04-01 16:08:46 -0300

    Atualização

--- filmes/src/pages/Favoritos/index.js
+++ filmes/src/pages/Favoritos/index.js
@@ -13,10 +13,19 @@ function Favoritos() {
 
     }, [])
 
+    function excluirFilme(id){
+        let filtroFilmes = filmes.filter((item) =>{
+            return(item.id !== id)
+        })
+
+        setFilmes(filtroFilmes);
+        localStorage.setItem("@primeflix", JSON.stringify(filtroFilmes))
+    }
+
     return (
         <div className='meus-filmes'>
             <h1>Meus filmes</h1>
-
+            {filmes.length === 0 && <span>Você não possui nenhum filme salvo :(</span>}
             <ul>
                 {filmes.map((item) => {
                     return (
@@ -24,7 +33,7 @@ function Favoritos() {
                             <span>{item.title}</span>
                             <div>
                                 <Link to={`/filme/${item.id}`}>Ver detalhes</Link>
-                                <button>Excluir</button>
+                                <button onClick={() => excluirFilme(item.id)}>Excluir</button>
                             </div>
                         </li>
                     )
